852666023ca9ac1ac4512cc8ce1ecaf8b6ec5156
AADDRRAA <raiferad1@gmail.com>
2024-02-27 23:26:47 +0200

    27/02part2

--- AuthenticationServer/Tools.py
+++ AuthenticationServer/Tools.py
@@ -1,4 +1,5 @@
 import base64
+import uuid
 
 class Tools:
     
@@ -34,3 +35,15 @@ class Tools:
         padded_data = byte_data + (padding_byte * padding_length)
 
         return padded_data
+    
+    def uuid_str_to_bytes(uuid_str):
+        # Convert the UUID string to a UUID object
+        uuid_obj = uuid.UUID(uuid_str)
+
+        # Get the bytes of the UUID object
+        uuid_bytes = uuid_obj.bytes
+
+        # Pad the UUID bytes to 16 bytes if necessary (usually not needed for standard UUIDs)
+        padded_uuid = uuid_bytes + b'\x00' * (16 - len(uuid_bytes))
+
+        return padded_uuid
